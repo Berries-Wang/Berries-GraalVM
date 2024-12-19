bfb302595d5939d270589055d4ebe61302e3ced3
Aca-S <a.stefanovic1999@gmail.com>
2024-12-19 17:38:54 +0100

    Analyze constant reflective method and constructor accessors

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/strictreflection/StrictConstantReflectionFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/strictreflection/StrictConstantReflectionFeature.java
@@ -23,7 +23,10 @@ import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -117,6 +120,10 @@ public class StrictConstantReflectionFeature implements InternalFeature {
             put(Utils.encodeMethodCall("java/lang/Class", "forName", "(Ljava/lang/String;)Ljava/lang/Class;"), StrictConstantReflectionFeature::classAccessHandler);
             put(Utils.encodeMethodCall("java/lang/Class", "getField", "(Ljava/lang/String;)Ljava/lang/reflect/Field;"), StrictConstantReflectionFeature::fieldAccessHandler);
             put(Utils.encodeMethodCall("java/lang/Class", "getDeclaredField", "(Ljava/lang/String;)Ljava/lang/reflect/Field;"), StrictConstantReflectionFeature::declaredFieldAccessHandler);
+            put(Utils.encodeMethodCall("java/lang/Class", "getConstructor", "([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;"), StrictConstantReflectionFeature::constructorAccessHandler);
+            put(Utils.encodeMethodCall("java/lang/Class", "getDeclaredConstructor", "([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;"), StrictConstantReflectionFeature::declaredConstructorAccessHandler);
+            put(Utils.encodeMethodCall("java/lang/Class", "getMethod", "(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;"), StrictConstantReflectionFeature::methodAccessHandler);
+            put(Utils.encodeMethodCall("java/lang/Class", "getDeclaredMethod", "(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;"), StrictConstantReflectionFeature::declaredMethodAccessHandler);
         }
     };
 
@@ -152,5 +159,52 @@ public class StrictConstantReflectionFeature implements InternalFeature {
 
     private static void declaredFieldAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext) {
         fieldAccessHandler(analyzerSuite, callContext, true);
+    }
+
+    private static void constructorAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext, boolean declaredOnly) {
+        Optional<Class<?>> clazz = analyzerSuite.classAnalyzer.inferConstant(getCallArg(callContext, 0), callContext.constantCalls);
+        Optional<ArrayList<Class<?>>> paramTypes = analyzerSuite.classArrayAnalyzer.inferConstant(getCallArg(callContext, 0), callContext.callSite, callContext.constantCalls);
+        if (clazz.isPresent() && paramTypes.isPresent()) {
+            Class<?>[] paramTypesArr = paramTypes.get().toArray(Class[]::new);
+            RuntimeReflection.registerConstructorLookup(clazz.get(), paramTypesArr);
+            try {
+                Constructor<?> constructor = declaredOnly ? clazz.get().getDeclaredConstructor(paramTypesArr) : clazz.get().getConstructor(paramTypesArr);
+                callContext.constantCalls.put(callContext.callSite, constructor);
+            } catch (NoSuchMethodException e) {
+                // The call will throw an exception during runtime - ignore for the rest of the analysis.
+            }
+        }
+    }
+
+    private static void constructorAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext) {
+        constructorAccessHandler(analyzerSuite, callContext, false);
+    }
+
+    private static void declaredConstructorAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext) {
+        constructorAccessHandler(analyzerSuite, callContext, true);
+    }
+
+    private static void methodAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext, boolean declaredOnly) {
+        Optional<Class<?>> clazz = analyzerSuite.classAnalyzer.inferConstant(getCallArg(callContext, 0), callContext.constantCalls);
+        Optional<String> methodName = analyzerSuite.stringAnalyzer.inferConstant(getCallArg(callContext, 1), callContext.constantCalls);
+        Optional<ArrayList<Class<?>>> paramTypes = analyzerSuite.classArrayAnalyzer.inferConstant(getCallArg(callContext, 2), callContext.callSite, callContext.constantCalls);
+        if (clazz.isPresent() && methodName.isPresent() && paramTypes.isPresent()) {
+            Class<?>[] paramTypesArr = paramTypes.get().toArray(Class[]::new);
+            RuntimeReflection.registerMethodLookup(clazz.get(), methodName.get(), paramTypesArr);
+            try {
+                Method method = declaredOnly ? clazz.get().getDeclaredMethod(methodName.get(), paramTypesArr) : clazz.get().getMethod(methodName.get(), paramTypesArr);
+                callContext.constantCalls.put(callContext.callSite, method);
+            } catch (NoSuchMethodException e) {
+                // The call will throw an exception during runtime - ignore for the rest of the analysis.
+            }
+        }
+    }
+
+    private static void methodAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext) {
+        methodAccessHandler(analyzerSuite, callContext, false);
+    }
+
+    private static void declaredMethodAccessHandler(AnalyzerSuite analyzerSuite, CallContext callContext) {
+        methodAccessHandler(analyzerSuite, callContext, true);
     }
 }
